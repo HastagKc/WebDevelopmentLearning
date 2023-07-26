7530342032ea890806be2b1deaab5d7994cb2fd9
HastagKc <chaudharykshittiz950@gmail.com>
2023-07-26 13:21:51 +0545

    function

--- js/PraticeOfControllFlow/1pr.js
+++ js/PraticeOfControllFlow/1pr.js
@@ -1,2 +1,40 @@
-// Check if a given number is even or odd.
+//! 1.Check if a given number is even or odd.
 let num = 10;
+if (num > 0) {
+  console.log("Positive");
+} else if (num == 0) {
+  console.log("Zero");
+} else {
+  console.log("Negative");
+}
+
+//? function in js
+//1. function functionName(para1, para2) {
+
+// }
+
+function multiplyNum(a, b) {
+  return a * b;
+}
+
+console.log(multiplyNum(10, 20));
+
+// 2. const functionName =function(){
+
+//}
+
+const addNum = function (a, b) {
+  return a + b;
+};
+
+let x = addNum(10, 25);
+console.log(x);
+
+// 3. arrow function in js
+
+const substractNum = (a, b) => {
+  return a - b;
+};
+
+let y = substractNum(50, 25);
+console.log(y);
