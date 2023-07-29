b37e870cbe31c9068564ad737d729c7d042d6939
HastagKc <chaudharykshittiz950@gmail.com>
2023-07-29 21:48:58 +0545

    july 29 complete

--- js/9.js
+++ js/9.js
@@ -1,12 +1,19 @@
 // and or and not operator in js
-let password = prompt("Enter any password");
+// let password = prompt("Enter any password");
 
 // And in js
 // where password.indexOf(" ") === -1 check if there is an
 // space in the input
 
-if (password.length >= 6 && password.indexOf(" ") === -1) {
-  console.log("Valid Password");
-} else {
-  console.log("Invalid Password");
-}
+// if (password.length >= 6 && password.indexOf(" ") === -1) {
+//   console.log("Valid Password");
+// } else {
+//   console.log("Invalid Password");
+// }
+
+// or in js
+let name = "kshittiz";
+// if value is not present then it will show -1 in the given string
+console.log(name.indexOf("-7")); // -1
+
+console.log(name.indexOf("h")); // 2 print index of value
