49cda480f257e6b8593fc58b6393625f48524f5d
HastagKc <chaudharykshittiz950@gmail.com>
2023-08-01 12:00:09 +0545

    aug-1

---
+++ js/11.js
@@ -0,0 +1,26 @@
+// // multi dimensinal array
+// let numbers = [
+//   [10, 20, 25],
+//   [70, 80, 35],
+//   [30, 20, 55],
+// ];
+
+// // accessing element from arrays
+
+// console.log(numbers);
+// console.log(numbers[1][0]); // 70
+
+// // modify element of an array
+// numbers[1][0] = "Kc";
+// console.log(numbers);
+
+// const array in js
+const myArray = [10, 20, 50, 40];
+myArray[0] = 20;
+myArray.unshift(45);
+myArray.push(85);
+
+console.log(myArray);
+
+// includes
+console.log(myArray.includes(20));
