6c06eba9f20e6b9cb20a30dae81cec2c60549c60
HastagKc <chaudharykshittiz950@gmail.com>
2023-08-02 17:54:35 +0545

    while loop

--- js/13.js
+++ js/13.js
@@ -8,9 +8,26 @@ for (let i = 1; i <= 5; i++) {
 }
 
 const seatPlanning = [
-  ["Rahul", "Sharmila", "Suresh"],
+  ["Rahul", "Sharmila", "Suresh", "Rahul-1", "Sharmila-2", "Suresh-3"],
   ["Dikamber", "Narayan", "Susil"],
   ["Ram", "Amit", "Bibek"],
 ];
 
-console.log(seatPlanning[0][1]);
+console.log(seatPlanning[0][1]); // Sharmila
+console.log(seatPlanning.length); // length is property not a method this will
+// give you length of the array
+
+for (let i = 0; i < seatPlanning.length; i++) {
+  for (let j = 0; j < seatPlanning[i].length; j++) {
+    console.log(seatPlanning[i][j]);
+  }
+}
+
+for (let i = 0; i < seatPlanning.length; i++) {
+  console.log(`Row no: ${i}`);
+  for (let j = 0; j < seatPlanning[i].length; j++) {
+    console.log(seatPlanning[i][j]);
+  }
+}
+
+console.log(seatPlanning[0].length); // 6
