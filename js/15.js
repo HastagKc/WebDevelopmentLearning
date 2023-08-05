26c2aca92e9c57ffa97e5cdce23f14b9a2a72b23
HastagKc <chaudharykshittiz950@gmail.com>
2023-08-05 16:23:54 +0545

    types of for loop

---
+++ js/15.js
@@ -0,0 +1,19 @@
+// for loop
+
+// making an array
+let nameList = ["Ram", "Hari", "Shyama", "Krishna"];
+for (let i = 0; i < nameList.length; i++) {
+  console.log(nameList[i]);
+}
+
+// using for of loop which is not support in internet explore
+
+for (let name of nameList) {
+  console.log(name);
+}
+
+let seatPlanning = [
+  ["Ram", "Hari", "Shyama", "Krishna"],
+  ["Pooja", "Sharmila", "Sujata", "Samina"],
+  ["Narayan", "Dk", "Samir"],
+];
