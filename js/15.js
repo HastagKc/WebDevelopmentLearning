6cdee37d339d57d7853d40755b3e66a9401eb09f
HastagKc <chaudharykshittiz950@gmail.com>
2023-08-05 16:30:28 +0545

    for of loop

--- js/15.js
+++ js/15.js
@@ -12,8 +12,25 @@ for (let name of nameList) {
   console.log(name);
 }
 
+console.log("----------------------");
 let seatPlanning = [
   ["Ram", "Hari", "Shyama", "Krishna"],
   ["Pooja", "Sharmila", "Sujata", "Samina"],
   ["Narayan", "Dk", "Samir"],
 ];
+
+console.log(seatPlanning[0].length);
+for (let i = 0; i < seatPlanning.length; i++) {
+  let row = seatPlanning[i];
+  for (let j = 0; j < row.length; j++) {
+    console.log(row[j]);
+  }
+}
+
+console.log("----------------------");
+
+for (let row of seatPlanning) {
+  for (let student of row) {
+    console.log(student);
+  }
+}
