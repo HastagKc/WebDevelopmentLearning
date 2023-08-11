7d3fc7692580f140a184d6f19fc7533031ddc6a8
HastagKc <chaudharykshittiz950@gmail.com>
2023-08-11 19:05:13 +0545

    map vs filter

--- js/25.js
+++ js/25.js
@@ -4,6 +4,23 @@ setTimeout(() => {
 }, 3000);
 
 // setInterval
-setInterval(() => {
-  console.log("Print after each 3 sec ");
-}, 3000);
+// setInterval(() => {
+//   console.log("Print after each 3 sec ");
+// }, 3000);
+
+// map() and fliter()
+
+// map() method returns the modify array having same length equal to
+// original array
+
+let numberList = [10, 20, 30, 40, 5, 60, 70, 80];
+const numbers = numberList.map(function (num) {
+  return num % 2 === 0;
+});
+console.log(numbers);
+
+const numbers2 = numberList.filter(function (num) {
+  return num % 2 === 0;
+});
+
+console.log(numbers2);
